0fa28a66dd340ed8f2774a850a53bbb1f2874499
val <ahumada.gonzalez.valeska@gmail.com>
2022-10-06 19:55:56 -0300

    lab 5

--- objects/02.why.py
+++ objects/02.why.py
@@ -1,10 +1,11 @@
 
 class Dog:
-    name = 'turbo'
-    age = 6
+  name = 'turbo'
+  age = 6
 
-    def sayName(self):
-        print(self.name)
+  def sayName(self):
+    print(self.name)
 
 doggie = Dog()
 doggie.sayName()
+ 
